27a5886b4c2e476fe8796ebe5269676db4d5ff26
Isapedro <isa.pedro92@hotmail.com>
2024-01-18 19:33:02 +0100

    html finished

---
+++ script.js
@@ -0,0 +1,7 @@
+const grid = document.querySelector(".grid");
+const stackBtn = document.querySelector(".stack");
+const scoreCounter = document.querySelector(".score-counter");
+const endGameScreen = document.querySelector(".end-game-screen");
+const endGameText = document.querySelector(".end-game-text");
+const playAgainAgain = document.querySelector(".play-again");
+
